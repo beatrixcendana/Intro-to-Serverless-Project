9f47bd394f90177be3217093fcd13cef6e5e4ac7
beatrixcendana <bcendana@uw.edu>
2021-06-23 21:44:55 -0700

    added conditional

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,12 +1,21 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const password = req.query.password || req.body.password
+    const password = req.query.password || (req.body && req.body.password)
+    // if condition: to check if the user's parameter input of password equals letmein.
+    // if it does, the output is "Access granted."
+    let your_response
+    if (password == "letmein") {
+        your_response = "Access granted.";
+    }
+
+    // if not equal the correct password, output "Acess denied."
+    else {
+        your_response = "Access denied.";
+    }
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: your_response
     };
 }
 
-// 1. gets content from a request parameter called password 
-// 2. returns it in the function's body

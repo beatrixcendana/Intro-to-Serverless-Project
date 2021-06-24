9035245b036755a348bd83bf9def48cab49aeaba
beatrixcendana <bcendana@uw.edu>
2021-06-24 14:10:02 -0700

    new twocatz function

--- twocatz/index.js
+++ twocatz/index.js
@@ -11,8 +11,12 @@ module.exports = async function (context, req) {
     // we need to receive it as a buffer since this is an image we are receiving from the API
     // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
     
+    var base64data = Buffer.from(data).toString('base64')
+//put what you want to turn into base64 inside "originaldata"
+//"originaldata" will be encoded in base64.
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: base64data
     };
 }

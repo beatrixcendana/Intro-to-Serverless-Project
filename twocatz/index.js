20218347bef15a8446a5a85fa9bce08340690554
beatrixcendana <bcendana@uw.edu>
2021-06-23 22:49:30 -0700

    added fetch

---
+++ twocatz/index.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch')
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    let resp = await fetch("https://cataas.com/cat/cute/says/Serverless", {
+        method: 'GET'
+    });
+    
+    let data = await resp.arrayBuffer()
+    // we need to receive it as a buffer since this is an image we are receiving from the API
+    // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
+    
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}

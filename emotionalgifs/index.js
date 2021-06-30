9edd3af66657735846c3425ff35b894629a489cc
beatrixcendana <bcendana@uw.edu>
2021-06-30 16:18:26 -0700

    updated emotional gifs function to send image to API and send emotion results in response

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,19 +1,51 @@
 var multipart = require('parse-multipart');
+var fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     // here's your boundary:
-    //var boundary = multipart.getBoundary(req.headers['content-type']);
+    var boundary = multipart.getBoundary(req.headers['content-type']);
     
     // TODO: assign the body variable the correct value
-    //var body = req.body
+    var body = req.body
 
     // parse the body
-    //var parts = multipart.Parse(body, boundary);
+    var parts = multipart.Parse(body, boundary);
+    
+    //we are not converting it.
+    var image = parts[0].data
 
-    //var base64data = Buffer.from(parts[0].data).toString('base64')
+    //now we are calling analyzeImage function
+    var result = await analyzeImage(image)
+
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: 'base64data'
+        body: result
     };
+}
+
+async function analyzeImage(img){
+    //change values while testing locally
+    const subscriptionKey = process.env.FaceAPI_KEY1;
+    const uriBase = process.env.FaceAPI_ENDPOINT + '/face/v1.0/detect';
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'   
+    })
+
+    //COMPLETE THE CODE
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST', 
+        body: img,  // img being passed into this function
+        
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    })
+    
+    let data = await resp.json();
+
+    return data; 
 }

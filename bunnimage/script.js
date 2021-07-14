c51e08a84c1cd14e7d39519c24e3bfa5c8d9a2c2
beatrixcendana <bcendana@uw.edu>
2021-07-13 17:11:34 -0700

    created form validation for correct or error information

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,7 +1,10 @@
-function getImage() {
-    //if the value is not blank
-    if (document.getElementById("name").value != '') {
-        // I am going to get the output of text and append the heart
-        $('#output').text(document.getElementById("name").value + "❤️")
+function getImage(event) {
+    event.preventDefault()
+    if (document.getElementById('name').value != '') {
+        $('#output').text("Thanks!")
+    }
+
+    else {
+        alert("No name error.")
     }
 }

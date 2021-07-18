72b4e5d29202d18fc2cc87c7d33730461135e5df
beatrixcendana <bcendana@uw.edu>
2021-07-17 23:03:20 -0700

    connected catzapp/index.html, catzapp/script.js, and your Azure function

--- catzapp/index.js
+++ catzapp/index.js
@@ -1,8 +1,25 @@
 function y1k3s() {
-    let name = document.getElementById("name").value
+    let name1 = document.getElementById("name1").value
+    let endpoint1= "https://cataas.com/cat/cute/says/" + name1
+    if(name1 != '') {
+        document.getElementById("image1").src = endpoint1 //call to API
+    }
 
-    let endpoint= "https://cataas.com/cat/cute/says/" + name
-    if(name != '') {
-        document.getElementById("image").src = endpoint //call to API
+    let name2 = document.getElementById("name2").value
+    let endpoint2= "https://cataas.com/cat/cute/says/" + name2
+    if(name2 != '') {
+        document.getElementById("image2").src = endpoint2 //call to API
+    }
+
+    let name3 = document.getElementById("name3").value
+    let endpoint3= "https://cataas.com/cat/cute/says/" + name3
+    if(name3 != '') {
+        document.getElementById("image3").src = endpoint3 //call to API
+    }
+
+    let name4 = document.getElementById("name4").value
+    let endpoint4= "https://cataas.com/cat/cute/says/" + name4
+    if(name4 != '') {
+        document.getElementById("image4").src = endpoint4 //call to API
     }
 }

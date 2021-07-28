742e3ff62c34006c082be7677032451ba2f80e1d
beatrixcendana <bcendana@uw.edu>
2021-07-28 09:55:15 -0700

    Week2 project with the additional image

---
+++ FoodReminder2/index.js
@@ -0,0 +1,31 @@
+const twiAccountSid = process.env.TWILIO_SID;
+const twiAuthToken = process.env.TWILIO_TOKEN;
+const client = require('twilio')(twiAccountSid, twiAuthToken);
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();    
+    if (myTimer.IsPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    client.messages
+    .create({ from: process.env.SENDER_NUMBER,
+           body: "It’s time to have lunch🍽️! What food/ingredients would you like to have?",
+           to: process.env.RECIPIENT_NUMBER,
+           mediaUrl: "https://dog.ceo/api/breeds/image/random",
+       })
+        .then(message => {             
+           context.log("Message sent");
+           context.res = {
+               body: 'Text successfully sent'
+           };
+           context.log('JavaScript timer trigger done!', timeStamp);
+           context.done();
+        }).catch(err => {
+          context.log.error("Twilio Error: " + err.message + " -- " + err.code);
+          context.res = {
+                   status: 500,
+                   body: `Twilio Error Message: ${err.message}\nTwilio Error code: ${err.code}`
+               };
+          context.done();
+        });
+    }

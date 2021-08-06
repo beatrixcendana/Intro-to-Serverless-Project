528ece3865fe0b76ba65b7d90901a7b7dd4173f5
beatrixcendana <bcendana@uw.edu>
2021-08-06 16:13:01 -0700

    try to connect HTTP trigger with timer function

--- TastyAPIRecipe/index.js
+++ TastyAPIRecipe/index.js
@@ -1,41 +1,31 @@
-const http = require("https");
-const querystring = require('querystring');
-const fetch = require('node-fetch');
+// const foodAPIKey = process.env.foodKey;
+// const foodAPIHost = process.env.foodHost;
+const fetch = require('node-fetch')
 module.exports = async function (context, req) {
-    context.log(req.body)
-    //const queryObject = querystring.parse(req.body);
+    context.log('JavaScript HTTP trigger function processed a request.');   
     const userInputRecipe = (req.query.userInputRecipe || (req.body && req.body.userInputRecipe));
-    const userTag = (req.query.tag || (req.body && req.body.tag));
-    const TastyAPIoptions = await fetch (userInputRecipe,userTag,{
+   // const userTag = (req.query.tag || (req.body && req.body.tag));
+   // const size = (req.query.size || (req.body && req.body.size));
+    let resultData = await fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients="+ userInputRecipe+"&number=1&ignorePantry=true&ranking=1", {
         "method": "GET",
-        "hostname": "tasty.p.rapidapi.com",
-        "port": null,
-        "path": "/recipes/list?from=0&size=1&tags=" + userTag + "&q=" +userInputRecipe,
         "headers": {
-            "x-rapidapi-key": "00f8d2ae4dmsh5da21a3af56a803p11c0b1jsn2fb3f647ffbd",
-            "x-rapidapi-host": "tasty.p.rapidapi.com",
-            "useQueryString": true
+            "x-rapidapi-key": process.env.foodKey,
+            "x-rapidapi-host": process.env.foodHost
         }
-    })
+    }).then(response => {
+            context.log(response);
+     })
+        .catch(err => {
+            console.error(err);
+    });     
     
-
-    const responseMessage = " ";
-    
-    const TastyAPIRequest = http.request(TastyAPIoptions, function (TastyAPIRequest) {
-        const chunks = [];
-        TastyAPIRequest.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-        TastyAPIRequest.on("end", function () {
-            const body = Buffer.concat(chunks);
-            console.log(body.toString()); //To be removed when response message is getting data
-            responseMessage = "User want a recipe for "+ userInputRecipe + 
-                        "and the recommended recipes are " + body.toString();
-        });
-    });
-    TastyAPIRequest.end();   
+    let recipeData = await resultData;
+    context.log(recipeData);
+    //Show result to response body
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        // status: 200, /* Defaults to 200 */      
+        //TODO: create objects for needed to show via Twilio API SMS, HTTP Timer trigger etc
+        body: recipeData
     };
+   
 }
